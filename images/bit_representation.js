0e8778b3c51e3ea0f58695efeaf89d978b4ff352
Aaron Spangler <aaron777@gmail.com>
2021-08-25 21:40:58 -0700

    Add bit_representation.png image.

---
+++ images/bit_representation.js
@@ -0,0 +1,12 @@
+// Source file to generate bit_representation.png
+// Used https://wavedrom.com/editor.html to convert.
+{signal: [
+  {name: 'binary 0', wave: 'lhl...'},
+  {name: 'binary 1', wave: 'l...hl'},
+ ], head: {
+   text:'Time (ms)',
+   tick:0,
+ }, foot: {
+   text:'Bit Representation',
+ },
+}
